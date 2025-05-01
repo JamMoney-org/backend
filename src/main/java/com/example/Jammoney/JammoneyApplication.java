b1b048290b26559359e99312cc1ff4606bfd9a2b
전선영 <minnie2002@naver.com>
2025-05-01 16:09:43 +0900

    첫번째 커밋

---
+++ src/main/java/com/example/Jammoney/JammoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jammoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JammoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jammoney/JammoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jammoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
