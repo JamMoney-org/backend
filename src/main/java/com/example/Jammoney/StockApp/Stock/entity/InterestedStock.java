44ad1c8b161e2c5101a88eca1966de0a16154045
minseok <minsuk6803@gmail.com>
2025-05-04 00:48:17 +0900

    InterestedStock 엔터티 구현 완료

---
+++ src/main/java/com/example/Jammoney/StockApp/Stock/entity/InterestedStock.java
@@ -0,0 +1,27 @@
+package com.example.Jammoney.StockApp.Stock.entity;
+import com.example.Jammoney.User.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class InterestedStock{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long interestedStockId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPANY_ID")
+    private Company company;
+}
