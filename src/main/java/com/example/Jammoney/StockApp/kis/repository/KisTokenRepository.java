d387089187dd519b17dec9cb8e7254cd3d439947
minseok <minsuk6803@gmail.com>
2025-05-06 03:02:26 +0900

    토큰 서버에 저장

---
+++ src/main/java/com/example/Jammoney/StockApp/kis/repository/KisTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.Jammoney.StockApp.kis.repository;
+import com.example.Jammoney.StockApp.kis.entity.KisToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KisTokenRepository extends JpaRepository<KisToken, Long> {
+}
+
