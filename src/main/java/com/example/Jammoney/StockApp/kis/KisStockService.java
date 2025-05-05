164e6842f2105a1fef2aaf36c6f7b9ccfbe746f9
minseok <minsuk6803@gmail.com>
2025-05-06 00:10:02 +0900

    주식 코드로 실시간 주가정보 가져오기

---
+++ src/main/java/com/example/Jammoney/StockApp/kis/KisStockService.java
@@ -0,0 +1,56 @@
+package com.example.Jammoney.StockApp.kis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class KisStockService {
+
+    private final KisAuthService kisAuthService;
+
+    @Value("${kis.app.key}")
+    private String appKey;
+
+    @Value("${kis.app.secret}")
+    private String appSecret;
+
+    @Value("${kis.base-url}")
+    private String baseUrl;
+
+    public Map<String, Object> getCurrentPrice(String stockCode) {
+        String url = baseUrl + "/uapi/domestic-stock/v1/quotations/inquire-price";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + kisAuthService.getAccessToken());
+        headers.set("appkey", appKey);
+        headers.set("appsecret", appSecret); // 일부 API는 secret 재사용
+
+
+        headers.set("tr_id", "FHKST01010100"); // 모의투자용 TR
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("fid_cond_mrkt_div_code", "J") // KOSPI: J, KOSDAQ: Q
+                .queryParam("fid_input_iscd", stockCode);  // ex: "005930"
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Map> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                Map.class
+        );
+
+        return response.getBody();
+    }
+}
+
