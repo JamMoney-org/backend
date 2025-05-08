11b4ff4240cfeb8dcbd781795785ff076612a3f0
minseok <minsuk6803@gmail.com>
2025-05-08 23:38:20 +0900

    사용자 인증처리를 위한 userDetails 구현

---
+++ src/main/java/com/example/jammoney/security/CustomeUserDetails.java
@@ -0,0 +1,57 @@
+package com.example.jammoney.security;
+
+import com.example.jammoney.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomeUserDetails extends User implements UserDetails {
+    private List<GrantedAuthority> authorities;
+    public CustomeUserDetails(User user) {
+        setId(user.getId());
+        setEmail(user.getEmail());
+        setPassword(user.getPassword());
+        List<String> roles = Collections.singletonList(user.getRole().name());
+        authorities = roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
