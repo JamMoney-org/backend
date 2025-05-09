ae6d8e83c188771ba6fc53897a850e8c428cf4b6
minseok <minsuk6803@gmail.com>
2025-05-09 23:03:05 +0900

    RefreshTokenRepository 커스터마이징

---
+++ src/main/java/com/example/jammoney/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.jammoney.auth.repository;
+
+import com.example.jammoney.auth.entity.RefreshToken;
+import com.example.jammoney.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByUser(User user);
+    void deleteByUser(User user);
+}
