d40831ecf6a0c87fbc7a7731b28caeca84e30426
minseok <minsuk6803@gmail.com>
2025-05-10 23:02:40 +0900

    PasswordMismatchException 찾음

--- src/main/java/com/example/jammoney/exception/PasswordMismatchException.java
+++ src/main/java/com/example/jammoney/exception/PasswordMismatchException.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 @Getter
 public class PasswordMismatchException extends RuntimeException {
     final ErrorCode errorCode;
-    private int a = 0;
     public PasswordMismatchException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
