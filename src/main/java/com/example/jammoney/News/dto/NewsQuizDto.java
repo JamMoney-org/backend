caab33a32fc145e4d0f44d134538a30252ea2539
김가현 <hyeonnni@naver.com>
2025-05-13 19:45:26 +0900

    [feat] 뉴스 엔터티 추가

--- src/main/java/com/example/jammoney/News/dto/NewsQuizDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.jammoney.news.dto;
-
-import java.util.List;
-
-public class NewsQuizDto {
-    private String question;
-    private List<String> choices;
-    private String correctAnswer;
-}
