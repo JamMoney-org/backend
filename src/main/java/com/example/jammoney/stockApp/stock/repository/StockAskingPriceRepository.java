4ada6ce7027a1ceb16c53e6edc3e6627fc62074a
minseok <minsuk6803@gmail.com>
2025-05-17 03:29:32 +0900

    호가 정보 저장을 위한 레포지토리 구성

---
+++ src/main/java/com/example/jammoney/stockApp/stock/repository/StockAskingPriceRepository.java
@@ -0,0 +1,10 @@
+package com.example.jammoney.stockApp.stock.repository;
+
+import com.example.jammoney.stockApp.stock.entity.StockAskingPrice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StockAskingPriceRepository extends JpaRepository<StockAskingPrice, Long> {
+    Optional<StockAskingPrice> findByCompanyId(Long stockId);
+}
---
+++ src/main/java/com/example/jammoney/stockApp/stock/repository/StockMinRepository.java
@@ -0,0 +1,7 @@
+package com.example.jammoney.stockApp.stock.repository;
+
+import com.example.jammoney.stockApp.stock.entity.StockMin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockMinRepository extends JpaRepository<StockMin, Long> {
+}
