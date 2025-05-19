1960af0176012d49de21d79033b796183232bfd9
minseok <minsuk6803@gmail.com>
2025-05-19 20:07:41 +0900

    매수 및 매도, 특정 사용자의 모든 주문 조회, 특정 사용자의 모든 보유 주식들 조회, 예약된 매수 및 매도 취소, 예약된 주문들 체결 처리 controller 구현

--- src/main/java/com/example/jammoney/stockApp/stock/controller/OrderController.java
+++ src/main/java/com/example/jammoney/stockApp/stock/controller/OrderController.java
@@ -1,42 +1,80 @@
 package com.example.jammoney.stockApp.stock.controller;
 
-import com.example.jammoney.stockApp.stock.service.HoldService;
-;
-import com.example.jammoney.stockApp.stock.dto.OrderRequestDto;
+import com.example.jammoney.stockApp.stock.dto.HoldingStockResponseDto;
+import com.example.jammoney.stockApp.stock.entity.Order;
+import com.example.jammoney.stockApp.stock.mapper.StockMapper;
 import com.example.jammoney.stockApp.stock.dto.OrderResponseDto;
+import com.example.jammoney.stockApp.stock.service.HoldingStockService;
 import com.example.jammoney.stockApp.stock.service.OrderService;
 import com.example.jammoney.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/stock")
 public class OrderController {
     private final OrderService orderService;
-    private final HoldService holdService;
-
-    public OrderController(OrderService orderService, HoldService holdService) {
-        this.orderService = orderService;
-        this.holdService = holdService;
-    }
+    private final HoldingStockService holdingStockService;
+    private final StockMapper stockMapper;
     @PostMapping("/buy")
-    public ResponseEntity<OrderResponseDto> buyStock(@RequestBody OrderRequestDto dto,
-                                                     @AuthenticationPrincipal User user) {
-        OrderResponseDto response = orderService.placeBuyOrder(dto, user);
-        return ResponseEntity.ok(response);
+    public ResponseEntity buyStocks(@RequestParam(name = "companyId") long companyId,
+                                    @RequestParam(name = "price") long price,
+                                    @RequestParam(name = "stockCount") int stockCount,
+                                    @AuthenticationPrincipal User user) {
+        Order order = orderService.buyStocks(user, companyId, price, stockCount);
+        OrderResponseDto stockOrderResponseDto = stockMapper.orderToDto(order);
+
+        return new ResponseEntity<>(stockOrderResponseDto, HttpStatus.CREATED);
     }
 
-    /**
-     * 매도 주문 API
-     */
+    //보유중인 특정 주식 매도
     @PostMapping("/sell")
-    public ResponseEntity<OrderResponseDto> sellStock(@RequestBody OrderRequestDto dto,
-                                                 @AuthenticationPrincipal User user) {
-        OrderResponseDto response = orderService.placeSellOrder(dto, user);
-        return ResponseEntity.ok(response);
+    public ResponseEntity sellStocks(@RequestParam(name = "companyId") long companyId,
+                                     @RequestParam(name = "price") long price,
+                                     @RequestParam(name = "stockCount") int stockCount,
+                                     @AuthenticationPrincipal User user) {
+        Order order = orderService.sellStocks(user, companyId, price, stockCount);
+        OrderResponseDto stockOrderResponseDto = stockMapper.orderToDto(order);
+
+        return new ResponseEntity<>(stockOrderResponseDto, HttpStatus.CREATED);
+    }
+
+    //특정 사용자의 모든 보유 주식들 조회
+    @GetMapping("/holdingStocks")
+    public ResponseEntity getHoldingStocks(@AuthenticationPrincipal User user) {
+        List<HoldingStockResponseDto> stockHoldResponseDtos = holdingStockService.findHoldingStocks(user.getId());
+        stockHoldResponseDtos = holdingStockService.setPercentage(stockHoldResponseDtos);
+
+        return new ResponseEntity<>(stockHoldResponseDtos, HttpStatus.OK);
+    }
+
+    //특정 사용자의 모든 주문들 조회
+    @GetMapping("/orders")
+    public ResponseEntity getOrders(@AuthenticationPrincipal User user) {
+        List<OrderResponseDto> stockOrderResponseDtos = orderService.getUserStockOrders(user.getId());
+
+        return new ResponseEntity<>(stockOrderResponseDtos, HttpStatus.OK);
+    }
+
+    // 예약된 매수, 매도 삭제
+    @DeleteMapping("/orders")
+    public void deleteStockOrders(@AuthenticationPrincipal User user,
+                                  @RequestParam("stockOrderId") long orderId,
+                                  @RequestParam("stockCount") int stockCount) {
+        orderService.deleteOrder(user, orderId, stockCount);
+    }
+
+    //예약된 주문들에 대해서 호가 확인해서 체결 처리
+    @GetMapping("/checkOrder")
+    public ResponseEntity checkOrder() {
+        orderService.checkOrder();
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
