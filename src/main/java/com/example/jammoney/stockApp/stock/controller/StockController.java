eab82858f4d5518c225aedb2ea095976067697e4
minseok <minsuk6803@gmail.com>
2025-05-19 19:52:56 +0900

    기존 StockController 기능 분리 및 코스피 지수 불러오는 기능 추가

--- src/main/java/com/example/jammoney/stockApp/stock/controller/StockController.java
+++ src/main/java/com/example/jammoney/stockApp/stock/controller/StockController.java
@@ -1,6 +1,7 @@
 package com.example.jammoney.stockApp.stock.controller;
 
 import com.example.jammoney.stockApp.kis.service.ApiCallService;
+import com.example.jammoney.stockApp.stock.dto.KospiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/stocks")
 @RequiredArgsConstructor
 public class StockController {
 
     private final ApiCallService apiCallService;
 
-    @GetMapping("/price/{code}")
-    public ResponseEntity<?> getPrice(@PathVariable String code) {
-        return ResponseEntity.ok(apiCallService.getCurrentPrice(code));
-    }
-
-    @GetMapping("/askingPrice/{code}")
-    public ResponseEntity<?> getAskingPrice(@PathVariable String code) {
-        return ResponseEntity.ok(apiCallService.getStockAskingPrice(code));
-    }
-
-    @GetMapping("/min/{code}/{time}")
-    public ResponseEntity<?> getMin(@PathVariable String code, @PathVariable String time) {
-        return ResponseEntity.ok(apiCallService.getStockMin(code, time));
+    @GetMapping("/kospi")
+    public ResponseEntity getKospiMonth() {
+        List<KospiResponseDto> kospiResponseDtos = apiCallService.getKospiMonthlyIndexThisYear();
+        return ResponseEntity.ok(kospiResponseDtos);
     }
 }
