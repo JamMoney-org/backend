9d44de3550deb57ec39855e7d773b8c714c39daa
minseok <minsuk6803@gmail.com>
2025-05-19 12:09:27 +0900

    모의투자 관련 에러 코드 추가

--- src/main/java/com/example/jammoney/exception/ErrorCode.java
+++ src/main/java/com/example/jammoney/exception/ErrorCode.java
@@ -1,6 +1,5 @@
 package com.example.jammoney.exception;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
@@ -11,7 +10,12 @@ public enum ErrorCode {
     REFRESH_TOKEN_NOT_FOUND("리프레시 토큰이 존재하지 않습니다.", 401),
     REFRESH_TOKEN_EXPIRED("리프레시 토큰이 만료되었습니다.", 401),
     CASH_NOT_FOUND("해당 사용자의 Cash 정보가 없습니다.", 411),
-    INSUFFICIENT_BALANCE("잔액이 부족합니다.", 412);
+    INSUFFICIENT_BALANCE("잔액이 부족합니다.", 412),
+    INSUFFICIENT_STOCK("보유 주식이 부족합니다.",413),
+    ORDER_NOT_FOUND("존재하지 않는 거래 내역입니다.",414),
+    ORDER_PERMISSION_DENIED("잘못된 삭제 요청입니다.",415),
+    ORDER_ALREADY_FINISH("이미 완료된 거래입니다.",416),
+    HOLDINGSTOCK_NOT_FOUND("보유하고 있지 않은 주식입니다.",417 );
     private final String message;
     private final int status;
 
