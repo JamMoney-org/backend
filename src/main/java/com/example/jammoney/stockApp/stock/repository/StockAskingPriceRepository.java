fafacd4938c503bb96f124a2a643d0908d05f512
minseok <minsuk6803@gmail.com>
2025-05-20 01:05:51 +0900

    jpa의 규약을 지키기위한 메서드 명 수정

--- src/main/java/com/example/jammoney/stockApp/stock/repository/StockAskingPriceRepository.java
+++ src/main/java/com/example/jammoney/stockApp/stock/repository/StockAskingPriceRepository.java
@@ -3,8 +3,6 @@ package com.example.jammoney.stockApp.stock.repository;
 import com.example.jammoney.stockApp.stock.entity.StockAskingPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface StockAskingPriceRepository extends JpaRepository<StockAskingPrice, Long> {
-    StockAskingPrice findByCompanyCompanyId(Long stockId);
+    StockAskingPrice findByCompany_CompanyId(Long stockId);
 }
--- src/main/java/com/example/jammoney/stockApp/stock/repository/StockMinRepository.java
+++ src/main/java/com/example/jammoney/stockApp/stock/repository/StockMinRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface StockMinRepository extends JpaRepository<StockMin, Long> {
-    List<StockMin> findAllByCompanyCompanyId(Long companyId);
+    List<StockMin> findAllByCompany_CompanyId(Long companyId);
     @Query(value = "SELECT * FROM stock_min s WHERE s.company_id = ?1 ORDER BY s.stock_min_id DESC LIMIT 420", nativeQuery = true)
     List<StockMin> findTop420ByCompanyIdOrderByStockMinIdDesc(Long companyId);
 }
