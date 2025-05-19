6a552d4e0a3247e1dcc2693ea4b63ec838ec7c7d
minseok <minsuk6803@gmail.com>
2025-05-19 12:04:59 +0900

    kis에서 분봉차트 정보를 받아오기 위한 dto 수정

--- src/main/java/com/example/jammoney/stockApp/kis/dto/StockMinDto.java
+++ src/main/java/com/example/jammoney/stockApp/kis/dto/StockMinDto.java
@@ -1,5 +1,9 @@
 package com.example.jammoney.stockApp.kis.dto;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 @Data
@@ -11,15 +15,42 @@ import java.util.List;
  */
 
 public class StockMinDto {
-    private List<ChartData> output2;
+    private StockMinOutput1 output1;
+    private List<StockMinOutput2> output2;
 
-    @Data
-    public static class ChartData {
-        private String stck_cntg_hour;   // 체결시간
-        private String stck_prpr;        // 현재가
-        private String stck_oprc;        // 시가
-        private String stck_hgpr;        // 고가
-        private String stck_lwpr;        // 저가
-        private String acml_vol;         // 거래량
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public class StockMinOutput1 {
+        //한글 종목 명
+        private String hts_kor_isnm;
+        //주식 현재가
+        private String stck_prpr;
+        //전일 대비
+        private String prdy_vrss;
+        //전일 대비율
+        private String prdy_ctrt;
+        //누적 거래량
+        private String acml_vol;
+        //누적 거래대금
+        private String acml_tr_pbmn;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class StockMinOutput2 {
+        //주식 체결 시간
+        private String stck_cntg_hour;
+        //주식 현재가
+        private String stck_prpr;
+        //주식 시가
+        private String stck_oprc;
+        //주식 최고가
+        private String stck_hgpr;
+        //주식 최저가
+        private String stck_lwpr;
+        //체결 거래량
+        private String cntg_vol;
     }
 }
