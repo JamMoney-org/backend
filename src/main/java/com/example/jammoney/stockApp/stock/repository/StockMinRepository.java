c5f54fb6b824561a246b44c4fd60b42cd335c8eb
minseok <minsuk6803@gmail.com>
2025-05-19 19:50:14 +0900

    stockMinId를 역순으로 조회해서 420개의 분봉 정보 가져오도록하는 기능 + 특정 회사에 대해 모든 분봉 정보 불러오는 기능 추가

--- src/main/java/com/example/jammoney/stockApp/stock/repository/StockMinRepository.java
+++ src/main/java/com/example/jammoney/stockApp/stock/repository/StockMinRepository.java
@@ -2,6 +2,12 @@ package com.example.jammoney.stockApp.stock.repository;
 
 import com.example.jammoney.stockApp.stock.entity.StockMin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StockMinRepository extends JpaRepository<StockMin, Long> {
+    List<StockMin> findAllByCompanyCompanyId(Long companyId);
+    @Query(value = "SELECT * FROM stock_min s WHERE s.company_id = ?1 ORDER BY s.stock_min_id DESC LIMIT 420", nativeQuery = true)
+    List<StockMin> findTop420ByCompanyIdOrderByStockMinIdDesc(Long companyId);
 }
