dbe03e8877fd11b875150b6a6b35706534c5d6fe
minseok <minsuk6803@gmail.com>
2025-05-20 00:57:06 +0900

    url 수정

--- src/main/java/com/example/jammoney/stockApp/stock/controller/InterestingStockController.java
+++ src/main/java/com/example/jammoney/stockApp/stock/controller/InterestingStockController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/interestingStocks")
+@RequestMapping("/api/interestingStocks")
 public class InterestingStockController {
     private final InterestingStockService interestingStockService;
     private final HoldingStockService holdingStockService;
