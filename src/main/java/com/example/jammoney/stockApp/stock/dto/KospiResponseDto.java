4390aa952a1c25ffc62aa528aa076643e1fbdcbc
minseok <minsuk6803@gmail.com>
2025-05-20 01:09:58 +0900

    주석 설명 추가

--- src/main/java/com/example/jammoney/stockApp/stock/dto/KospiResponseDto.java
+++ src/main/java/com/example/jammoney/stockApp/stock/dto/KospiResponseDto.java
@@ -1,7 +1,10 @@
 package com.example.jammoney.stockApp.stock.dto;
 
 import lombok.Data;
-
+/**
+ * KOSPI 월봉 정보 응답 DTO
+ * 주식 차트(월봉) 조회 시 클라이언트에 전달되는 데이터 구조
+ */
 @Data
 public class KospiResponseDto {
     //현재 달 (YYYYMM)
--- src/main/java/com/example/jammoney/stockApp/stock/dto/UserPortfolioResponseDto.java
+++ src/main/java/com/example/jammoney/stockApp/stock/dto/UserPortfolioResponseDto.java
@@ -2,7 +2,10 @@ package com.example.jammoney.stockApp.stock.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-
+/**
+ * 사용자 포트폴리오 요약 응답 DTO
+ * 대시보드나 마이페이지 등에서 사용자의 전체 자산 상태를 요약하여 보여줄 때 사용
+ */
 @Getter
 @Builder
 public class UserPortfolioResponseDto {
