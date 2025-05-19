0660d4f08b2ff66b421cbdc5f21cb9845dd81caa
minseok <minsuk6803@gmail.com>
2025-05-20 00:57:47 +0900

    kospi 지수 가져오는 controller 명 수정

--- src/main/java/com/example/jammoney/stockApp/stock/controller/KospiController.java
+++ src/main/java/com/example/jammoney/stockApp/stock/controller/KospiController.java
@@ -5,7 +5,6 @@ import com.example.jammoney.stockApp.stock.dto.KospiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +12,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class StockController {
+@RequestMapping("/api/kospi")
+public class KospiController {
 
     private final ApiCallService apiCallService;
 
-    @GetMapping("/kospi")
+    //kospi의 2025년 1월 ~ 현재 달까지의 월봉 정보를 가져옴
+    @GetMapping()
     public ResponseEntity getKospiMonth() {
         List<KospiResponseDto> kospiResponseDtos = apiCallService.getKospiMonthlyIndexThisYear();
         return ResponseEntity.ok(kospiResponseDtos);
