c62e85d9f1b251b215150b005f70d28c6105605e
minseok <minsuk6803@gmail.com>
2025-05-20 11:45:01 +0900

    서버 실행 초기에, 모의투자에 사용할 회사를 DB에 넣는 Controller 구현

---
+++ src/main/java/com/example/jammoney/stockApp/stock/controller/CompanyInitController.java
@@ -0,0 +1,24 @@
+package com.example.jammoney.stockApp.stock.controller;
+
+import com.example.jammoney.stockApp.stock.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/init")
+@RequiredArgsConstructor
+public class CompanyInitController {
+
+    private final CompanyService companyService;
+
+
+    //모의투자에서 사용할 회사의 이름과 종목코드를 받아서 DB에 저장함 (서버 실행시 딱 한 번만 실행하는 메서드)
+    @PostMapping
+    public ResponseEntity<Void> initialize() throws InterruptedException {
+        companyService.fillCompaniesData();
+        return ResponseEntity.ok().build();
+    }
+}
