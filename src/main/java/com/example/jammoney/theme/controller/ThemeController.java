826efa3d8ef94b3a550940c23d01a577c0e8eea1
김가현 <hyeonnni@naver.com>
2025-05-20 10:36:08 +0900

    도커파일 생성, 테마별 학습 주석 추가

--- src/main/java/com/example/jammoney/theme/controller/ThemeController.java
+++ src/main/java/com/example/jammoney/theme/controller/ThemeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-public class ThemeController {
+public class ThemeController { //카테고리 선택
     private final ThemeService themeService;
 
     @GetMapping("/api/themes")
--- src/main/java/com/example/jammoney/theme/controller/TopicController.java
+++ src/main/java/com/example/jammoney/theme/controller/TopicController.java
@@ -12,19 +12,22 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-public class TopicController {
+public class TopicController { //카테고리 선택 후 토픽 리스트 띄움
     private final LearningTopicService topicService;
 
+    //토픽 리스트
     @GetMapping("/api/themes/{themeId}/topics")
     public List<TopicListDto> getTopics(@PathVariable Long themeId) {
         return topicService.getAllTopics(themeId);
     }
 
+    //토픽 선택시 자세한 내용
     @GetMapping("/api/themes/{themeId}/topics/{topicId}/details")
     public TopicDetailDto getDetails(@PathVariable long topicId) {
         return topicService.getTopicDetail(topicId);
     }
 
+    //내용 생성
     @PostMapping("/api/themes/topics")
     public void createTopic(@RequestBody TopicCreateDto createDto) {
         topicService.createTopic(createDto);
