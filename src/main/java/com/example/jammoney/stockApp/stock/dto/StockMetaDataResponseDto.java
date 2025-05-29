64c08aab7ee030b99660f6b87afc3c24cfb22261
minseok <minsuk6803@gmail.com>
2025-05-29 17:36:43 +0900

    프론트에 메타데이터 보낼 dto 구축 (보강 필요)

---
+++ src/main/java/com/example/jammoney/stockApp/stock/dto/StockMetaDataResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.jammoney.stockApp.stock.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StockMetaDataResponseDto {
+    private String hts_avls;         // 시가총액
+    private String stck_fcam;        // 액면가
+    private String lstn_stcn;        // 상장 주식 수
+    private String bscp_kor_isnm;    // 업종명
+    private String eps;              // EPS
+    private String per;              // PER
+    private String bps;              // BPS
+    private String pbr;              // PBR
+    private String stac_month;       // 결산월
+
+}
