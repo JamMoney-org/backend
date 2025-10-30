dfe4065093b2ee683324aea49365d2195b44bd43
minseok <minsuk6803@gmail.com>
2025-10-30 18:13:59 +0900

    feat : StringRedisTemplate 기반으로 변경

--- src/main/java/com/example/jammoney/config/RedisConfig.java
+++ src/main/java/com/example/jammoney/config/RedisConfig.java
@@ -3,30 +3,15 @@ package com.example.jammoney.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
-        RedisTemplate<String, String> t = new RedisTemplate<>();
-        t.setConnectionFactory(cf);
-        StringRedisSerializer s = new StringRedisSerializer();
-        t.setKeySerializer(s);
-        t.setValueSerializer(s);
-        t.setHashKeySerializer(s);
-        t.setHashValueSerializer(s);
-        t.afterPropertiesSet();
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory cf) {
+        StringRedisTemplate t = new StringRedisTemplate(cf);
+        t.setEnableTransactionSupport(true);
         return t;
     }
 }
