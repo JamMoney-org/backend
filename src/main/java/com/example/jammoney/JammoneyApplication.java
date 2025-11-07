491ee560fd2e6d982997d7ab2ed5eeab3ffcfeee
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-11-07 18:19:25 +0900

    refactor: 뉴스 저장 기능 추가 (#152)
    * [chore] ci 수정
    
    * 자동 배포 확인 구현
    
    * cd.yml 수정
    
    * cors 수정
    
    * refactor: 뉴스 저장 기능 추가

--- src/main/java/com/example/jammoney/JammoneyApplication.java
+++ src/main/java/com/example/jammoney/JammoneyApplication.java
@@ -2,6 +2,7 @@ package com.example.jammoney;
 
 import com.example.jammoney.news.crawler.FinanceNewsCrawler;
 import com.example.jammoney.news.dto.NewsRequestDto;
+import com.example.jammoney.news.service.NewsService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,4 +19,26 @@ public class JammoneyApplication {
 		SpringApplication.run(JammoneyApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(FinanceNewsCrawler crawler, NewsService newsService) {
+		return args -> {
+			List<NewsRequestDto> newsList = crawler.fetchTodayNews();
+			System.out.println("크롤링된 뉴스 개수: " + newsList.size());
+			for (NewsRequestDto dto : newsList) {
+				System.out.println("제목: " + dto.getTitle());
+				System.out.println("출처: " + dto.getSource());
+				System.out.println("날짜: " + dto.getPublishDate());
+
+				// 본문 일부 출력
+				String snippet = dto.getContent().length() > 100
+						? dto.getContent().substring(0, 100) + "..."
+						: dto.getContent();
+				System.out.println("내용: " + snippet);
+
+				// 요약 기능 제거됨
+				System.out.println("------");
+			}
+      newsService.saveNewsList(newsList);
+		};
+	}
 }
